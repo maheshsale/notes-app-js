7e9caa84e6122c17e19539d23af625c3ae7b9e68
Mahesh Sale <maheshsale333@gmail.com>
2023-11-02 18:37:47 +0530

    Updated READMI

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# notes-app-js
-Notes App using JavaScript
+# Notes App using JavaScript
+
+Author: Mahesh Sale
+Date: 02 Nov 2023
+
+<hr>
+# Description
+This notes app is developed as a part of practicing JavaScript.
+
+The whole code is developed by myself (no video is referred for code).
+
+The app focuses more on functionality than styling.
+
+More styling will be added in future.
+
+
--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-function deleteNote(){
-
-}
+let notesDiv = document.querySelector(".notes");
 
 function createNote(){
-      let notesDiv = document.querySelector(".notes");
-      console.log(notesDiv)
-
       // Create a new p tag
       let note = document.createElement("p");
       note.setAttribute("contenteditable", "true");
       let img = document.createElement("img");
       img.src = "./images/delete.png"
+
       img.addEventListener("click", function(event){
-            event.target.parentElement.remove();
+            if(confirm("Confirm to delete the note")){
+                  event.target.parentElement.remove();
+            }
       });
+
       note.appendChild(img);
-
       notesDiv.appendChild(note);
-
-      console.log(note);
 }
